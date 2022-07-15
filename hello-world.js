3a73609addef576190c43f65228272963278f9ca
Amanda <85843625+amandavdc@users.noreply.github.com>
2022-07-15 20:04:55 +0700

    Update hello-world.js

--- hello-world.js
+++ hello-world.js
@@ -1,3 +1,12 @@
-let text = "Hello World!"
-
-console.log(text)
+<html>
+<head>
+  <title>HELLO WORLD!</title>
+</head>
+<body>
+  <main>
+    <h1>WEBSITE UTAMA AMANDA</h1> 
+    <h2>Selebihnya cek pada situs github yh</h2> 
+    <p>Cek MY <a href="https://github.com/amandavdc">GITHUB</a></p>
+  </main>
+</body>
+</html>
